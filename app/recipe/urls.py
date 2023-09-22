66176938d6bfdafc90bf98c15349f84c1cfa40f3
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-09-22 16:28:31 +0300

    Update URL routing and admin panel for recipe app

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,11 +7,11 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register("", views.RecipeViewSet)
-router.register("tags", views.TagViewSet)
-
 app_name = "recipes"
+
+router = DefaultRouter()
+router.register("recipes", views.RecipeViewSet)
+router.register("tags", views.TagViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
