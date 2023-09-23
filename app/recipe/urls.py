dd76901988902d8db59f209121d7e3d0847493e0
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-09-23 12:37:36 +0300

    Add IngredientViewSet to urls

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -12,6 +12,7 @@ app_name = "recipes"
 router = DefaultRouter()
 router.register("recipes", views.RecipeViewSet)
 router.register("tags", views.TagViewSet)
+router.register("ingredients", views.IngredientViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
