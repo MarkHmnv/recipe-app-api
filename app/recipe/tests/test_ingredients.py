e36172fe44e5b92924e65742d34b954e2d5b49a7
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-09-24 15:00:56 +0300

    Refactor test utils into shared methods for better organization
    Moved create_recipe, create_tag, and create_ingredient methods from app/recipe/tests/test_recipe_api.py to app/recipe/tests/utils.py, allowing for shared use across multiple test files. This change improves code organization, reduces redundancy, and allows for easier future modifications. Furthermore, additional tests have been added to filter recipes by ingredients and tags, thus enhancing current functionality.

--- app/recipe/tests/test_ingredients.py
+++ app/recipe/tests/test_ingredients.py
@@ -1,22 +1,20 @@
 """
 Tests for ingredients API
 """
+from decimal import Decimal
+
 from django.urls import reverse
 from django.test import TestCase
 
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 
 from recipe.serializers import IngredientSerializer
 from recipe.tests import utils
 
 INGREDIENTS_URL = reverse("recipes:ingredient-list")
-
-
-def create_ingredient(user, name="Ingredient", **params):
-    return Ingredient.objects.create(user=user, name=name, **params)
 
 
 class PublicIngredientsApiTests(TestCase):
@@ -40,8 +38,8 @@ class PrivateIngredientsApiTests(TestCase):
 
     def test_retrieve_ingredients(self):
         """Test retrieving a list of ingredients"""
-        create_ingredient(user=self.user)
-        create_ingredient(user=self.user)
+        utils.create_ingredient(user=self.user)
+        utils.create_ingredient(user=self.user)
 
         res = self.client.get(INGREDIENTS_URL)
 
@@ -54,8 +52,8 @@ class PrivateIngredientsApiTests(TestCase):
     def test_ingredients_limited_to_user(self):
         """Test list of ingredients is limited to authenticated user"""
         other_user = utils.create_user(email="other@example.com", password="password")
-        create_ingredient(user=other_user)
-        ingredient = create_ingredient(user=self.user)
+        utils.create_ingredient(user=other_user)
+        ingredient = utils.create_ingredient(user=self.user)
 
         res = self.client.get(INGREDIENTS_URL)
 
@@ -63,4 +61,32 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], ingredient.name)
         self.assertEqual(res.data[0]["id"], ingredient.id)
+
+    def test_filter_ingredients_assigned_to_recipe(self):
+        """Test listing ingredients by those assigned to recipes"""
+        in1 = utils.create_ingredient(user=self.user, name="Ingredient 1")
+        in2 = utils.create_ingredient(user=self.user, name="Ingredient 2")
+        recipe = utils.create_recipe(user=self.user, title="Recipe 1")
+        recipe.ingredients.add(in1)
+
+        res = self.client.get(INGREDIENTS_URL, {"assigned_only": 1})
+
+        s1 = IngredientSerializer(in1)
+        s2 = IngredientSerializer(in2)
+        self.assertIn(s1.data, res.data)
+        self.assertNotIn(s2.data, res.data)
+
+    def test_filtered_ingredients_unique(self):
+        """Test filtered ingredients returns unique items"""
+        ing = utils.create_ingredient(user=self.user, name="Ingredient 1")
+        utils.create_ingredient(user=self.user, name="Ingredient 2")
+        recipe1 = utils.create_recipe(user=self.user, title="Recipe 1")
+        recipe1.ingredients.add(ing)
+        recipe2 = utils.create_recipe(user=self.user, title="Recipe 2")
+        recipe2.ingredients.add(ing)
+
+        res = self.client.get(INGREDIENTS_URL, {"assigned_only": 1})
+
+        self.assertEqual(len(res.data), 1)
+
 
