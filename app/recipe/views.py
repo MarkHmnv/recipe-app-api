e90bf017741064b93a1712b6ca685202a5765eb9
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-09-24 15:01:12 +0300

    Add filtering to recipe API
    Enhancements were made to the recipe API views to include more sophisticated filtering. The BaseRecipeAttrViewSet and RecipeViewSet classes now support filtering attributes such as 'assigned_only', 'tags', and 'ingredients'. This change provides users with more flexibility when retrieving recipes and attributes, specifically those assigned to recipes or tagged in a specific way. OpenAPI documentation was also updated to reflect these changes.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,13 @@
 """
 Views for the recipe APIs
 """
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes
+)
+
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -17,21 +24,69 @@ class BaseViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "assigned_only",
+                OpenApiTypes.INT, enum=[0, 1],
+                description="Filter by items assigned to recipes",
+            )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(BaseViewSet):
     """Base view set for recipe attributes"""
     def get_queryset(self):
         """Retrieve attribute for authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get("assigned_only", 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name').distinct()
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "tags",
+                OpenApiTypes.STR,
+                description="Filter by tag IDs, separated by commas",
+            ),
+            OpenApiParameter(
+                "ingredients",
+                OpenApiTypes.STR,
+                description="Filter by ingredient IDs, separated by commas",
+            )
+        ]
+    )
+)
 class RecipeViewSet(BaseViewSet):
     """View for manage recipe APIs"""
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
 
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
         """Retrieve recipes for authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user).order_by("-id").distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request"""
